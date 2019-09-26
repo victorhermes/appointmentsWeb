e2c58fe4bdf23d3d1c2b6f765489b64210c9349d
Victor <me.victorhermes@gmail.com>
2019-09-26 20:19:28 -0300

    signin

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignIn() {
-    return <h1>SignIn</h1>;
+    return (
+        <>
+            <img src={logo} alt="Logo" />
+
+            <form action="">
+                <input type="email" placeholder="Digite seu e-mail" />
+                <input type="password" placeholder="Digite sua senha" />
+
+                <button type="submit">Enviar</button>
+
+                <Link to="/signup">Cadastrar conta</Link>
+            </form>
+        </>
+    );
 }
