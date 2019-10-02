9877a358dd6b3307eec0b880b76d5aa3c7e35623
Victor <me.victorhermes@gmail.com>
2019-10-01 23:18:33 -0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './config/ReactotronConfig';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import store from './store';
+import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
+
+import store from './store';
 
 import GlobalStyle from './styles/global';
 
