cdd38b1bea21024e01e60545a7c06d3ac94f455c
Victor <me.victorhermes@gmail.com>
2019-10-28 22:25:03 -0300

    notifications

--- src/pages/Notifications/index.js
+++ src/pages/Notifications/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdNotifications } from 'react-icons/md';
-import { Container, Badge } from './styles';
+import { Container, Badge, NotificationList, Notification } from './styles';
 
 export default function Notifications() {
     return (
@@ -8,6 +8,24 @@ export default function Notifications() {
             <Badge hasUnread>
                 <MdNotifications color="#7159c1" size={20} />
             </Badge>
+
+            <NotificationList>
+                <Notification unread>
+                    <p>Um novo agendamento foi concluído</p>
+                    <time>há 1 dia</time>
+                    <button type="button">Marcar como lido</button>
+                </Notification>
+                <Notification>
+                    <p>Um novo agendamento foi concluído</p>
+                    <time>há 1 dia</time>
+                    <button type="button">Marcar como lido</button>
+                </Notification>
+                <Notification>
+                    <p>Um novo agendamento foi concluído</p>
+                    <time>há 1 dia</time>
+                    <button type="button">Marcar como lido</button>
+                </Notification>
+            </NotificationList>
         </Container>
     );
 }
