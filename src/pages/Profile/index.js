98862e5e384191a2e5f511091cadeb18041f104a
Victor Hermes <me.victorhermes@gmail.com>
2019-11-20 21:32:29 -0300

    profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-    return <h1>Profile</h1>;
+    return (
+        <Container>
+            <Form>
+                <Input name="name" placeholder="Nome completo" />
+                <Input name="email" placeholder="Seu e-mail" />
+
+                <hr />
+
+                <Input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Senha antiga"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirmação de senha"
+                />
+
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+
+            <button type="submit">Sair do sistema</button>
+        </Container>
+    );
 }
